180be28df3e1b4c00dc733e020a8a0fef15d2bd9
Vishal Cody <vy0052704@gmail.com>
2026-08-28 12:53:33 +0530

    Run email worker with API service

--- src/api.ts
+++ src/api.ts
@@ -4,20 +4,33 @@ import { logger } from "./config/logger.js";
 import { ensureEmailIndex } from "./lib/elasticsearch.js";
 import { prisma } from "./lib/prisma.js";
 import { redis } from "./lib/redis.js";
+import { emailWorker } from "./modules/emails/email.worker.js";
 import { emailQueue } from "./queues/email.queue.js";
 import { reconcileScheduledEmails } from "./queues/reconcile.js";
 
 await ensureEmailIndex();
 await reconcileScheduledEmails();
-const server = app.listen(env.API_PORT, () => logger.info({ port: env.API_PORT }, "API listening"));
+
+const server = app.listen(env.API_PORT, () => {
+  logger.info({ port: env.API_PORT }, "API listening");
+});
 
 async function shutdown(signal: string) {
   logger.info({ signal }, "Graceful API shutdown started");
+
   server.close(async () => {
-    await Promise.allSettled([emailQueue.close(), redis.quit(), prisma.$disconnect()]);
+    await Promise.allSettled([
+      emailWorker.close(),
+      emailQueue.close(),
+      redis.quit(),
+      prisma.$disconnect()
+    ]);
+
     process.exit(0);
   });
+
   setTimeout(() => process.exit(1), 15_000).unref();
 }
+
 process.on("SIGTERM", () => void shutdown("SIGTERM"));
 process.on("SIGINT", () => void shutdown("SIGINT"));
